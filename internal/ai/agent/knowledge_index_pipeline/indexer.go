8f432e72d42df92baa32abb89831dd96f89de3f3
Morning <springoflotus@gmail.com>
2026-01-19 11:03:04 +0800

    feat：实现知识库 agent

---
+++ internal/ai/agent/knowledge_index_pipeline/indexer.go
@@ -0,0 +1,12 @@
+package knowledge_index_pipeline
+
+import (
+	"context"
+	indexer2 "github.com/NuyoahCh/eocall/internal/ai/indexer"
+	"github.com/cloudwego/eino/components/indexer"
+)
+
+// newIndexer component initialization function of node 'RedisIndexer' in graph 'KnowledgeIndexing'
+func newIndexer(ctx context.Context) (idr indexer.Indexer, err error) {
+	return indexer2.NewMilvusIndexer(ctx)
+}
